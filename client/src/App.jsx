b66df252d33b5ccce7c37664dd0d61000435274f
ssb-shree <bavachikarshree1666@gmail.com>
2025-03-02 18:29:58 +0530

    added browser route to react app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+import Homepage from "./pages/Homepage"
+
+import Editorpage from "./pages/Editorpage"
+
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Homepage/>} />
+          <Route path={"/editor/:roomID"} element={<Editorpage/>} />
+        </Routes>
+      </BrowserRouter> 
+    </>
   )
 }
 
